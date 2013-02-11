d652e3a96c5c710876665e4facb43b197096285b
adrianppg <adrianppg3@yahoo.es>
2013-02-11 01:22:36 +0100

    modify part5

--- src/hw1_part5.rb
+++ src/hw1_part5.rb
@@ -1,27 +1,20 @@
 ### Part V - advanced OOP
 
 class Class
-    def attr_accessor_with_history(attr_name)
-        attr_name = attr_name.to_s
+  
+  def attr_accessor_with_history(attr_name)
+        attr_name = attr_name.to_s # make sure it's a string
         attr_reader attr_name
-        attr_reader attr_name + '_history'
-        puts "feo"
-        puts attr_name+"_history"
-        
-        puts @bar
-        class_eval %Q{
-          def #{attr_name}= (attr_name)
-            @attr_name = attr_name
-            if (@attr_name==nil)
-              @attr_name+'_history'=Array.new
-              @attr_name+"_history".push nil
-            else
-              @attr_name+'_history'.push attr_name  
-            end            
-          end
-          def #{attr_name}
-            @attr_name
-          end
-        }
-    end
+        attr_reader attr_name+"_history"
+        class_eval %Q"
+            def #{attr_name}=(value)
+                if !defined? @#{attr_name}_history
+                    @#{attr_name}_history = [@#{attr_name}]
+                end
+                @#{attr_name} = value
+                @#{attr_name}_history << value
+            end
+        "
+    end  
+    
 end
